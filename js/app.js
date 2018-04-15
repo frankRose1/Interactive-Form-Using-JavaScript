5926b95fe8ccf061a9b69549acbb31150683cce9
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-04-15 18:27:17 -0400

    now when user selects a shirt theme, only the associated colors will appear in the dropdown menu

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ const fieldSet1 = document.querySelectorAll('fieldset')[0];
 const nameInput = document.getElementById('name');
 const jobRole = document.getElementById('title');
 const jobOptions = jobRole.children;
+//t-shirt section
+const shirtDesign = document.getElementById('design');
+const shirtColor = document.getElementById('color');
+
 
 
 // When the page loads, give focus to the first text field
@@ -26,3 +30,45 @@ jobRole.addEventListener('change', (e) => {
       fieldSet1.appendChild(otherLabel);
     }
 });
+
+
+//t-shirt color options
+const cornFlowerBlue = document.querySelector('option[value ="cornflowerblue"]');
+const darkSlateGrey = document.querySelector('option[value ="darkslategrey"]');
+const gold = document.querySelector('option[value ="gold"]');
+
+const tomato = document.querySelector('option[value ="tomato"]');
+const steelBlue = document.querySelector('option[value ="steelblue"]');
+const dimGrey = document.querySelector('option[value ="dimgrey"]');
+function removeColorOptions() {
+  shirtColor.removeChild(cornFlowerBlue);
+  shirtColor.removeChild(darkSlateGrey);
+  shirtColor.removeChild(gold);
+  shirtColor.removeChild(tomato);
+  shirtColor.removeChild(steelBlue);
+  shirtColor.removeChild(dimGrey);
+}
+removeColorOptions();
+// If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
+// If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," and "Dim Grey."
+
+ shirtDesign.addEventListener('change', (e) => {
+   if (e.target.value == 'js puns') {
+     shirtColor.appendChild(cornFlowerBlue);
+     shirtColor.appendChild(darkSlateGrey);
+     shirtColor.appendChild(gold);
+
+     shirtColor.removeChild(tomato);
+     shirtColor.removeChild(steelBlue);
+     shirtColor.removeChild(dimGrey);
+   }
+   if (e.target.value == 'heart js') {
+     shirtColor.appendChild(tomato);
+     shirtColor.appendChild(steelBlue);
+     shirtColor.appendChild(dimGrey);
+
+     shirtColor.removeChild(cornFlowerBlue);
+     shirtColor.removeChild(darkSlateGrey);
+     shirtColor.removeChild(gold);
+   }
+ });
