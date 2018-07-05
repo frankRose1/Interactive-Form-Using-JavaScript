0691aaf6f1a212c521d5c3f9b1fd4be43e22ade2
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-07-05 00:34:25 -0400

    added event listener to show/hide payment fields based on the option selected

--- scripts/app2.js
+++ scripts/app2.js
@@ -10,6 +10,9 @@ NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn
     elem.on(name, fn);
   });
 };
+////////////////////////////////////////
+
+$('input#name').focus(); //focus on the first input on page load
 
 //1)SHOW/HIDE 'OTHER' FIELD
 //hide the other field  and show it if other option is selected
@@ -34,16 +37,64 @@ $('#title').on('change', showOtherField);
   const chooseOption = `<option value="choosedesign">&larr; Choose a design!</option>`;
   colorMenu.innerHTML = chooseOption;
 
+  function jsPunsOptions(){
+    const punsOptions = `
+      <option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>
+      <option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option>
+      <option value="gold">Gold (JS Puns shirt only)</option>
+      `;
+      colorMenu.innerHTML = punsOptions;
+  }
+
+  function heartJsOptions(){
+    const heartOptions = `
+      <option value="tomato">Tomato (I &#9829; JS shirt only)</option>
+      <option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option>
+      <option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>
+    `;
+    colorMenu.innerHTML = heartOptions;
+  }
+
   function createShirtOptions(e){
     if(e.target.value === "js puns"){
-      console.log('puns');
+      jsPunsOptions();
     } else if (e.target.value === "heart js") {
-      console.log('heart');
+      heartJsOptions();
     } else {
       colorMenu.innerHTML = chooseOption;
     }
   }
 
   $('select#design').on('change', createShirtOptions);
+
+
+  // PAYMENT SECTION
+    //default to the credit card option
+    //show the apprpriate div if the related option is selected cc, bitcoin, paypal
+    const creditCardDiv = $('div#credit-card');
+    const paypalDiv = $('div#paypal');
+    const bitcoinDiv = $('div#bitcoin');
+    paypalDiv.style.display = 'none';
+    bitcoinDiv.style.display = 'none';
+
+    function togglePaymentDivs(show, hide1, hide2){
+      show.style.display = 'block';
+      hide1.style.display = 'none';
+      hide2.style.display = 'none';
+    }
+    
+    function showPaymentFields(e){
+      if (e.target.value === 'credit card') {
+        togglePaymentDivs(creditCardDiv, paypalDiv, bitcoinDiv);
+      } else if (e.target.value === 'bitcoin') {
+        togglePaymentDivs(bitcoinDiv, paypalDiv, creditCardDiv);
+      } else if (e.target.value === 'paypal') {
+        togglePaymentDivs(paypalDiv, bitcoinDiv, creditCardDiv);
+      } else {
+        console.log('show an alert div!');
+      }
+    }
+
+  $('select#payment').on('change', showPaymentFields);
 
 
