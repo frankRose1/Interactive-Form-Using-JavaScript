ad8c32f2970513e8cfb9249964be99a4173b135d
Frank Rosendorf <frank.rosendorf1@gmail.com>
2019-03-07 21:16:59 -0500

    Add tooltips and real time validation on certain fields

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-//////START BLING.JS
+// START BLING.JS
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 Node.prototype.on = window.on = function (name, fn) {
@@ -10,11 +10,18 @@ NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn
     elem.on(name, fn);
   });
 };
-//////END BLING.JS
+// END BLING.JS
 
-$('input#name').focus();
+const emailRegex = /^[^@]+@[^@]+\.[a-z]+$/;
+const nameRegex = /^[a-zA-Z\s]+$/;
+const cvvRegex = /^\d{3}$/;
+const zipRegex = /^\d{5}$/;
+const ccRegex = /^\d{13,16}$/
+$('#name').focus();
 
-//1)JOB ROLE SECTION --> show/hide 'other' field
+/**
+ * Job Role Section - show/hide input if 'other' option is selected
+ */
 const otherJobInput = $('input#other-title');
 otherJobInput.style.display = 'none';
 
@@ -27,131 +34,152 @@ function showOtherField(e){
 }
 
 $('#title').on('change', showOtherField);
+// ===============================================================//
 
-//2) T-SHIRT SECTION --> hide color opions until selection is made in the design select menu
-  const colorMenu = $('select#color');
-  const chooseOption = `<option value="choosedesign">&larr; Choose a design!</option>`;
-  colorMenu.innerHTML = chooseOption;
 
-  function jsPunsOptions(){
-    const punsOptions = `
-      <option value="cornflowerblue">Cornflower Blue</option>
-      <option value="darkslategrey">Dark Slate Grey</option>
-      <option value="gold">Gold</option>
-      `;
-      colorMenu.innerHTML = punsOptions;
-  }
+/**
+ * T-shirt section - hide color opions until selection is made in the design select menu
+ */
+const colorMenu = $('select#color');
+const chooseOption = `<option value="choosedesign">&larr; Choose a design!</option>`;
+colorMenu.innerHTML = chooseOption;
 
-  function heartJsOptions(){
-    const heartOptions = `
-      <option value="tomato">Tomato</option>
-      <option value="steelblue">Steel Blue</option>
-      <option value="dimgrey">Dim Grey</option>
+function jsPunsOptions(){
+  const punsOptions = `
+    <option value="cornflowerblue">Cornflower Blue</option>
+    <option value="darkslategrey">Dark Slate Grey</option>
+    <option value="gold">Gold</option>
     `;
-    colorMenu.innerHTML = heartOptions;
+    colorMenu.innerHTML = punsOptions;
+}
+
+function heartJsOptions(){
+  const heartOptions = `
+    <option value="tomato">Tomato</option>
+    <option value="steelblue">Steel Blue</option>
+    <option value="dimgrey">Dim Grey</option>
+  `;
+  colorMenu.innerHTML = heartOptions;
+}
+
+function createShirtOptions(e){
+  if(e.target.value === "js puns"){
+    jsPunsOptions();
+  } else if (e.target.value === "heart js") {
+    heartJsOptions();
+  } else {
+    colorMenu.innerHTML = chooseOption;
+  }
+}
+
+$('select#design').on('change', createShirtOptions);
+// ===============================================================//
+
+
+/**
+ * Activities section - if conflicting events are selected then the other checkboxes need to be disabled
+ */
+const activitesCheckboxes = $$('.activity-cb');
+const totalPriceDiv = document.createElement('div');
+let totalPrice = 0;
+totalPriceDiv.innerHTML = `Your total price: <span class="money-green">$${totalPrice}.00</span>`;
+$('fieldset.activities').appendChild(totalPriceDiv);
+
+function activitesHandler() {
+  //frameworks and express conflict
+  if(this.parentElement.textContent.includes('Tuesday 9am-12pm')){
+    activitesCheckboxes.forEach(cb => {
+      if(cb.parentElement.textContent.includes('Tuesday 9am-12pm') && cb !== this && this.checked){
+        cb.disabled = true;
+        cb.parentElement.classList.add('disabled');
+      } else if (cb.parentElement.textContent.includes('Tuesday 9am-12pm') && cb !== this && !this.checked) {
+        cb.disabled = false;
+        cb.parentElement.classList.remove('disabled');
+      }
+    });
+  }
+  //libraries and node  js conflict
+  if(this.parentElement.textContent.includes('Tuesday 1pm-4pm')){
+    activitesCheckboxes.forEach(cb => {
+      if(cb.parentElement.textContent.includes('Tuesday 1pm-4pm') && cb !== this && this.checked){
+        cb.disabled = true;
+        cb.parentElement.classList.add('disabled');
+      } else if(cb.parentElement.textContent.includes('Tuesday 1pm-4pm') && cb !== this && !this.checked) {
+        cb.disabled = false;
+        cb.parentElement.classList.remove('disabled');
+      }
+    });
+  }    
+}
+
+//main conferernce is $200 everything else is $100
+function sumActivities(){
+  //if checked add
+  if (this.name !== 'all' && this.checked) {
+    totalPrice += 100;
+  } else if (this.name === 'all' && this.checked) {
+    totalPrice += 200;
+  }
+  //if unchecked subtract
+  if (this.name !== 'all' && !this.checked) {
+    totalPrice -= 100;
+  } else if (this.name === 'all' && !this.checked) {
+    totalPrice -= 200;
   }
 
-  function createShirtOptions(e){
-    if(e.target.value === "js puns"){
-      jsPunsOptions();
-    } else if (e.target.value === "heart js") {
-      heartJsOptions();
-    } else {
-      colorMenu.innerHTML = chooseOption;
-    }
+  if (totalPrice < 0) {
+    totalPrice = 0;
   }
-
-  $('select#design').on('change', createShirtOptions);
-
-  //3)ACTIVITIES SECTION --> if conflicting events are selected then the other checkboxes need to be disabled
-  const activitesCheckboxes = $$('.activity-cb');
-  const totalPriceDiv = document.createElement('div');
-  let totalPrice = 0;
   totalPriceDiv.innerHTML = `Your total price: <span class="money-green">$${totalPrice}.00</span>`;
-  $('fieldset.activities').appendChild(totalPriceDiv);
+}
 
-  function activitesHandler() {
-    //frameworks and express conflict
-    if(this.parentElement.textContent.includes('Tuesday 9am-12pm')){
-      activitesCheckboxes.forEach(cb => {
-        if(cb.parentElement.textContent.includes('Tuesday 9am-12pm') && cb !== this && this.checked){
-          cb.disabled = true;
-          cb.parentElement.classList.add('disabled');
-        } else if (cb.parentElement.textContent.includes('Tuesday 9am-12pm') && cb !== this && !this.checked) {
-          cb.disabled = false;
-          cb.parentElement.classList.remove('disabled');
-        }
-      });
-    }
-    //libraries and node  js conflict
-    if(this.parentElement.textContent.includes('Tuesday 1pm-4pm')){
-      activitesCheckboxes.forEach(cb => {
-        if(cb.parentElement.textContent.includes('Tuesday 1pm-4pm') && cb !== this && this.checked){
-          cb.disabled = true;
-          cb.parentElement.classList.add('disabled');
-        } else if(cb.parentElement.textContent.includes('Tuesday 1pm-4pm') && cb !== this && !this.checked) {
-          cb.disabled = false;
-          cb.parentElement.classList.remove('disabled');
-        }
-      });
-    }    
+activitesCheckboxes.on('change', activitesHandler);
+activitesCheckboxes.on('change', sumActivities);
+// ===============================================================//
+
+
+
+/**
+ * Payment Section
+ *  show the appropriate payment option(credit, bitcoin, or paypal) depending on what option is selected
+ *  default to credit card
+ */
+const creditCardDiv = $('div#credit-card');
+const paypalDiv = $('div#paypal');
+const bitcoinDiv = $('div#bitcoin');
+const alertUserDiv = $('div.alert-user');
+$('option[value="credit card"]').selected = true;
+
+function togglePaymentDivs(show, hide1, hide2, hide3){
+  show.style.display = 'block';
+  hide1.style.display = 'none';
+  hide2.style.display = 'none';
+  hide3.style.display = 'none';
+}
+
+togglePaymentDivs(creditCardDiv, paypalDiv, bitcoinDiv, alertUserDiv); //show the credit card div by default
+
+function showPaymentFields(e){
+  if (e.target.value === 'credit card') {
+    togglePaymentDivs(creditCardDiv, paypalDiv, bitcoinDiv, alertUserDiv);
+  } else if (e.target.value === 'bitcoin') {
+    togglePaymentDivs(bitcoinDiv, paypalDiv, creditCardDiv, alertUserDiv);
+  } else if (e.target.value === 'paypal') {
+    togglePaymentDivs(paypalDiv, bitcoinDiv, creditCardDiv, alertUserDiv);
+  } else {
+    togglePaymentDivs(alertUserDiv, bitcoinDiv, creditCardDiv, paypalDiv);
   }
+}
 
-  //main conferernce is $200 everything else is $100
-  function sumActivities(){
-    //if checked add
-    if (this.name !== 'all' && this.checked) {
-      totalPrice += 100;
-    } else if (this.name === 'all' && this.checked) {
-      totalPrice += 200;
-    }
-    //if unchecked subtract
-    if (this.name !== 'all' && !this.checked) {
-      totalPrice -= 100;
-    } else if (this.name === 'all' && !this.checked) {
-      totalPrice -= 200;
-    }
+$('select#payment').on('change', showPaymentFields);
+// ===============================================================//
 
-    if (totalPrice < 0) {
-      totalPrice = 0;
-    }
-    totalPriceDiv.innerHTML = `Your total price: <span class="money-green">$${totalPrice}.00</span>`;
-  }
 
-  activitesCheckboxes.on('change', activitesHandler);
-  activitesCheckboxes.on('change', sumActivities);
+/**
+ * Form Validation
+ *  The following section will validate the form on submit
+ */
 
-  //4) PAYMENT SECTION --> show the appropriate div if the related option is selected cc, bitcoin, paypal
-  const creditCardDiv = $('div#credit-card');
-  const paypalDiv = $('div#paypal');
-  const bitcoinDiv = $('div#bitcoin');
-  const alertUserDiv = $('div.alert-user');
-  $('option[value="credit card"]').selected = true;
-
-  function togglePaymentDivs(show, hide1, hide2, hide3){
-    show.style.display = 'block';
-    hide1.style.display = 'none';
-    hide2.style.display = 'none';
-    hide3.style.display = 'none';
-  }
-
-  togglePaymentDivs(creditCardDiv, paypalDiv, bitcoinDiv, alertUserDiv); //show the credit card div by default
-
-  function showPaymentFields(e){
-    if (e.target.value === 'credit card') {
-      togglePaymentDivs(creditCardDiv, paypalDiv, bitcoinDiv, alertUserDiv);
-    } else if (e.target.value === 'bitcoin') {
-      togglePaymentDivs(bitcoinDiv, paypalDiv, creditCardDiv, alertUserDiv);
-    } else if (e.target.value === 'paypal') {
-      togglePaymentDivs(paypalDiv, bitcoinDiv, creditCardDiv, alertUserDiv);
-    } else {
-      togglePaymentDivs(alertUserDiv, bitcoinDiv, creditCardDiv, paypalDiv);
-    }
-  }
-
-  $('select#payment').on('change', showPaymentFields);
-
-//5) FORM VALIDATION
 function addInvalidText(element, message){
   element.classList.add('invalid-text');
   element.textContent = message;
@@ -177,28 +205,25 @@ function removeInvalidHighlighter(input, labelTarget, message){
   inputLabel.textContent = message;
 }
 
-//name input should only be letters and spaces, and cant be blank
-function checkName(e){
+function validateName(e){
   const nameInput = $('input#name');
-  if (nameInput.value.trim().length === 0 || !(/^[a-zA-Z\s]*$/).test(nameInput.value)) {
+  if (!nameRegex.test(nameInput.value)) {
     e.preventDefault();
-    highlightInvalidInput(nameInput, 'name', 'Please enter your name, no numbers or symbols!');
+    highlightInvalidInput(nameInput, 'name', 'Please enter your name, no numbers or special characters!');
   } else {
     removeInvalidHighlighter(nameInput, 'name', 'Name:');
   }
 }
 
-  //email cant be blank and must be a properly formatted email
-  function checkEmail(e){
-    const emailInput = $('#mail');
-    const emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (emailInput.value.trim().length === 0 || !emailRegEx.test(emailInput.value)) {
-      e.preventDefault();
-      highlightInvalidInput(emailInput, 'mail', 'Please enter a valid email address');
-    } else {
-      removeInvalidHighlighter(emailInput, 'mail', 'Email:');
-    }
+function validateEmail(e){
+  const emailInput = $('#mail');
+  if (!emailRegex.test(emailInput.value)) {
+    e.preventDefault();
+    highlightInvalidInput(emailInput, 'mail', 'Please enter a valid email address');
+  } else {
+    removeInvalidHighlighter(emailInput, 'mail', 'Email:');
   }
+}
 
 //have a counter that ticks down each time an activity box is unchecked
     //if counter hits zero then we know all boxes are unchecked and we must alert user
@@ -213,7 +238,7 @@ function checkActivities(e){
   });
   if (cbCounter === 0) {
     e.preventDefault();
-    addInvalidText(activitiesLegend, 'Please choose atleast one activity!');
+    addInvalidText(activitiesLegend, 'Please choose at least one activity!');
   } else if (cbCounter > 0) {
     removeInvalidText(activitiesLegend, 'Register for Activities');
   }
@@ -230,24 +255,23 @@ function isPaymentSelected(e){
 }
 
 const ccOption = $('option[value="credit card"]');
-//between 13 and 16 digits numbers only
+
 function validateCreditCard(e) {
   const ccInput = $('#cc-num');
   if (ccOption.selected) {
-    if (isNaN(ccInput.value) || ccInput.value.trim().length < 13 || ccInput.value.trim().length > 16) {
+    if (!ccRegex.test(ccInput.value)) {
       e.preventDefault();
-      highlightInvalidInput(ccInput, 'cc-num', 'Please enter a 13-16 digit card number');
+      highlightInvalidInput(ccInput, 'cc-num', 'Enter a 13-16 digit card number');
     } else {
       removeInvalidHighlighter(ccInput, 'cc-num', 'Card Number:');
     }
   }
 }
 
-//five digits numbers only
 function validateZipCode(e){
   const zipCodeInput = $('#zip');
   if (ccOption.selected) {
-    if (isNaN(zipCodeInput.value) || zipCodeInput.value.trim().length !== 5) {
+    if (!zipRegex.test(zipCodeInput.value)) {
       e.preventDefault();
       highlightInvalidInput(zipCodeInput, 'zip', 'Enter a 5 digit zip code');
     } else {
@@ -256,11 +280,10 @@ function validateZipCode(e){
   }
 }
 
-//three digits numbers only
 function validateCVV(e){
   const cvvInput = $('#cvv');
   if (ccOption.selected) {
-    if(isNaN(cvvInput.value) || cvvInput.value.trim().length !== 3){
+    if(!cvvRegex.test(cvvInput.value)){
       e.preventDefault();
       highlightInvalidInput(cvvInput, 'cvv', 'Enter a 3 digit cvv');
     } else {
@@ -270,14 +293,75 @@ function validateCVV(e){
 }
 
 function validateForm(e){
-  checkName(e);
-  checkEmail(e);
+  validateName(e);
+  validateEmail(e);
   checkActivities(e);
   isPaymentSelected(e);
   validateCreditCard(e);
   validateZipCode(e);
   validateCVV(e);
+  console.log(e)
 }
 
-$('form').on('submit', validateForm);
+$('form').addEventListener('submit', validateForm);
+// ===============================================================//
+
+
+/**
+ * Realtime Validation
+    shows a tooltip if a field is invalid, updates as user provides input
+ */
+function showOrHideTip(show, element){
+  if (show){
+    element.style.display = 'inherit';
+  } else {
+    element.style.display = 'none'
+  }
+}
+
+function showOrRemoveHighlight(show, element){
+  if(show){
+    element.classList.add('invalid');
+  } else {
+    element.classList.remove('invalid');
+  }
+}
+
+function isValidName(name){
+  return nameRegex.test(name)
+}
+
+function isValidEmail(email){
+  return emailRegex.test(email)
+}
+
+function isValidCvv(cvv){
+  return cvvRegex.test(cvv)
+}
+
+function isValidZip(zip){
+  return zipRegex.test(zip);
+}
+
+function isValidCC(ccNum){
+  return ccRegex.test(ccNum)
+}
+
+function createEventListener(validator){
+  return e => {
+    const text = e.target.value;
+    const valid = validator(text);
+    const alertUser = text !== '' && !valid;
+    const tooltip = e.target.nextElementSibling;
+    showOrRemoveHighlight(alertUser, e.target)
+    showOrHideTip(alertUser, tooltip);
+  }
+}
+
+$('#mail').addEventListener('input', createEventListener(isValidEmail))
+$('#name').addEventListener('input', createEventListener(isValidName))
+$('#cvv').addEventListener('input', createEventListener(isValidCvv))
+$('#zip').addEventListener('input', createEventListener(isValidZip))
+$('#cc-num').addEventListener('input', createEventListener(isValidCC))
+// ===============================================================//
 
